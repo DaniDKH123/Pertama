b2c40bfac0a09949068ca239c059bd4ecce2f8ca
DaniDKH123 <dani.kasmanto112@gmail.com>
2022-04-19 12:02:20 +0700

    commit

--- app/src/main/java/com/rspb/pertama/MoveWithObjectActivity.kt
+++ app/src/main/java/com/rspb/pertama/MoveWithObjectActivity.kt
@@ -2,10 +2,22 @@ package com.rspb.pertama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MoveWithObjectActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_PERSON = "extra_pertama"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move_with_object)
+
+        val tvobject : TextView = findViewById(R.id.tv_object_received)
+
+        val person = intent.getParcelableExtra<Pertama>(EXTRA_PERSON) as Pertama
+
+        val text = "Name : ${person.name.toString()}, \nEmail : ${person.email}, \nAge : ${person.age}, \nLocation : ${person.city}"
+            tvobject.text = text
     }
 }
--- app/src/main/java/com/rspb/pertama/Pertama.kt
+++ app/src/main/java/com/rspb/pertama/Pertama.kt
@@ -1,3 +1,40 @@
 package com.rspb.pertama
 
-data class pertama()
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Pertama(
+    val name: String?,
+    val age: Int?,
+    val email: String?,
+    val city: String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readValue(Int::class.java.classLoader) as? Int,
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel,flags: Int) {
+        parcel.writeString(name)
+        parcel.writeValue(age)
+        parcel.writeString(email)
+        parcel.writeString(city)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Pertama> {
+        override fun createFromParcel(parcel: Parcel): Pertama {
+            return Pertama(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Pertama?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
